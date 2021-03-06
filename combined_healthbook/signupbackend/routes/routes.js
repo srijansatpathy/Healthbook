c4dcb993530c62b7feff3ceef6b27082f5b75946
shilong zhang <shawnzsl130@gmail.com>
2021-03-05 22:55:05 -0800

    backend server added, but haven't linked

---
+++ combined_healthbook/signupbackend/routes/routes.js
@@ -0,0 +1,26 @@
+const { response } = require('express')
+const express = require('express')
+const router = express.Router()
+const signupTemplateCopy = require('../models/SignupModels')
+const bcrypt = require('bcrypt')
+
+router.post('/signup', async (request, response) => {
+
+    const saltpassword = await bcrypt.genSalt(10)
+    const securePassword = await bcrypt.hash(request.body.password, saltpassword)
+
+    const signupUser = new signupTemplateCopy({
+        username:request.body.username,
+        email:request.body.email,
+        password:securePassword
+    })
+    signupUser.save()
+    .then(data =>{
+        response.json(data)
+    })
+    .catch(error =>{
+        response.json(errpr)
+    })
+})
+
+module.exports = router
