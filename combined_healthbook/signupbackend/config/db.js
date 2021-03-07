71dd9aa2913c01f60a167831334e93b326d4fd79
shawnzsl130@gmail.com <shawn@Makeblade.localdomain>
2021-03-06 22:23:01 -0800

    another minor signin bug fixed

--- combined_healthbook/signupbackend/config/db.js
+++ combined_healthbook/signupbackend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DATABASE_ACCESS_KEVIN, {
+    const conn = await mongoose.connect(process.env.DATABASE_ACCESS, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true
